d410a8347d50e9ecfd555df5e7fe91b8aa49fb6d
Javier Priego Ramirez <Javier.Priego.Ramirez@unit4.com>
2020-08-22 22:12:30 +0200

    Order de solution

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Robots.ASDSAD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var robots = new List<Robot>();
+            var robot1 = new Robot();
+            robots.Add(robot1);
+
+            foreach (IRobot robot in robots)
+            {
+                robot.DoStuff();
+            }
+        }
+    }
+
+    public class Robot
+        : IRobot
+    {
+        public void DoStuff()
+        {
+            Console.WriteLine("Robot init");
+            Console.WriteLine("Dance");
+        }
+    }
+
+    public interface IRobot
+    {
+        void DoStuff();
+    }
+}
